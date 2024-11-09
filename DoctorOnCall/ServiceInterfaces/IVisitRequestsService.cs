c37a93e80b730f2b266d4fb39995075adfb36fb4
Nazar <example"gmail.com>
2024-11-09 11:23:46 +0200

    Services were added

---
+++ DoctorOnCall/ServiceInterfaces/IVisitRequestsService.cs
@@ -0,0 +1,8 @@
+﻿using DoctorOnCall.Models;
+
+namespace DoctorOnCall.RepositoryInterfaces;
+
+public interface IVisitRequestsService
+{
+    public Task<VisitRequestModel> CreateVisitRequest(VisitRequestModel visitRequest);
+}
---
+++ DoctorOnCall/Services/VisitRequestsService.cs
@@ -0,0 +1,18 @@
+﻿using DoctorOnCall.Models;
+using DoctorOnCall.RepositoryInterfaces;
+
+namespace DoctorOnCall.Services;
+
+public class VisitRequestsService : IVisitRequestsService
+{
+    private readonly IVisitRequestRepository _visitRequestRepository;
+
+    public VisitRequestsService(IVisitRequestRepository visitRequestRepository)
+    {
+        _visitRequestRepository = visitRequestRepository;
+    }
+    public async Task<VisitRequestModel> CreateVisitRequest(VisitRequestModel visitRequest)
+    {
+        return  await _visitRequestRepository.AddVisitRequest(visitRequest);
+    }
+}
