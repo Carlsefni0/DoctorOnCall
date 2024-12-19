049b3cbfe896822d16d904d5d1b25c1c3522e592
Nazar <example"gmail.com>
2024-12-20 01:10:31 +0200

    refactor: code improvement and configuration

---
+++ DoctorOnCall/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,85 @@
+﻿using DoctorOnCall.Models;
+using DoctorOnCall.Repositories;
+using DoctorOnCall.RepositoryInterfaces;
+using DoctorOnCall.Services;
+using DoctorOnCall.Utils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PatientOnCall.RepositoryInterfaces;
+
+namespace DoctorOnCall.Extensions;
+
+public static class ApplicationServiceExtension
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+
+
+        // DbContext
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+        // Identity
+        services.AddIdentity<AppUser, AppRole>()
+            .AddEntityFrameworkStores<DataContext>()
+            .AddDefaultTokenProviders();
+
+        // Репозиторії
+        services.AddScoped<IVisitRequestRepository, VisitRequestRepository>();
+        services.AddScoped<IDoctorRepository, DoctorRepository>();
+        services.AddScoped<IPatientRepository, PatientRepository>();
+        services.AddScoped<IVisitRequestMedicineRepository, VisitRequestMedicineRepository>();
+
+        // Сервіси
+        services.AddScoped<IVisitService, VisitService>();
+        services.AddScoped<IDoctorService, DoctorService>();
+        services.AddScoped<IPatientService, PatientService>();
+        services.AddScoped<IMedicineService, MedicineService>();
+        services.AddScoped<IAuthService, AuthService>();
+
+        // EmailService як Singleton (якщо він не залежить від DbContext)
+        services.AddSingleton<IEmailService, EmailService>();
+
+        // TokenService як Singleton, якщо не залежить від DbContext
+        services.AddScoped<ITokenService, TokenService>();
+
+        // ActionContextAccessor
+        services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+        // АвтоMapper
+        services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+        return services;
+    }
+
+}
---
+++ DoctorOnCall/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using DoctorOnCall.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DoctorOnCall.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+            
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var tokenKey = config["Jwt:Key"] ?? throw new ApplicationException("Missing JWT Key");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config["Jwt:Issuer"],
+                    ValidAudience = config["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(tokenKey))
+                };
+            });
+        services.AddAuthorization();
+        return services;
+    }
+}
