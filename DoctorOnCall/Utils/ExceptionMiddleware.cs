f326518396824b9967d06fd068b4a98f83fcded7
Nazar <example"gmail.com>
2024-12-20 01:12:42 +0200

    feat: improved error handling

---
+++ DoctorOnCall/Utils/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Authentication;
+
+namespace DoctorOnCall.Utils;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (NotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (InvalidCredentialException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (AuthenticationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { Message = "An unexpected error occurred." });
+        }
+    }
+}
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message) { }
+}
+
