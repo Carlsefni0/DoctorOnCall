8c37e6414b8f120b99675952753f95e052621a90
Nazar <example"gmail.com>
2025-03-01 00:21:32 +0200

    refactor: unnecessary DTOs removed

--- DoctorOnCall/DTOs/VisitRequestDTOs/VisitRequestFilterDto.cs
+++
@@ -1,8 +0,0 @@
-﻿using DoctorOnCall.Enums;
-
-public class VisitRequestFilterDto
-{
-    public int? Year { get; set; }
-    public int? Month { get; set; }
-    public VisitRequestStatus? Status { get; set; }
-}
