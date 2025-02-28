c1483ace0cbe233537c329afbec5976cd3c12c71
Nazar <example"gmail.com>
2025-03-01 01:26:22 +0200

    feat: paged DTO added

---
+++ DoctorOnCall/DTOs/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace DoctorOnCall.DTOs.ResponseDto;
+
+public class PagedResult<T>
+{
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public ICollection<T> Items { get; set; }
+}
