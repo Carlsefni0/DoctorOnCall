0a51e884bd05a85aed017763f1eeebf3b198feb3
Nazar <example"gmail.com>
2025-03-03 13:59:44 +0200

    feat: functionality to view visit requests assigned to a doctor

---
+++ DoctorOnCallFrontend/src/pages/assigned_requests/AssignedRequests.jsx
@@ -0,0 +1,109 @@
+﻿import { useState } from "react";
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import {
+    createViewDay,
+    createViewMonthAgenda,
+    createViewMonthGrid,
+    createViewWeek, viewMonthGrid,
+    viewWeek,
+} from "@schedule-x/calendar";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import { createEventsServicePlugin } from "@schedule-x/events-service";
+import "@schedule-x/theme-default/dist/index.css";
+import StyledPaper from "../../components/StyledPaper.jsx";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import performRequest from "../../utils/performRequest.js";
+import {queryClient} from "../../utils/queryClient.js";
+
+const formatDateTime = (isoString) => {
+    const date = new Date(isoString);
+    return date.toISOString().slice(0, 16).replace("T", " ");
+};
+
+export default function AssignedRequests() {
+    const assignedVisitRequests = useLoaderData();
+    const navigate = useNavigate();
+    const eventsService = useState(() => createEventsServicePlugin())[0]
+    
+    const events = assignedVisitRequests.map(request => ({
+        id: String(request.visitRequestId),
+        title: request.visitRequestType,
+        start: formatDateTime(request.startDateTime),
+        end: formatDateTime(request.endDateTime),
+        location: request.address,
+        description: "Double click on the visit to get more information",
+        calendarId: "event",
+    }))
+    
+    const calendar = useCalendarApp({
+        views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+        defaultView: viewMonthGrid.name,
+        events,
+        callbacks: {
+            onRangeUpdate: async (range) => {
+                try {
+                    const apiBaseUrl = import.meta.env.VITE_BACKEND_URL;
+                    
+                    console.log(range);
+
+                    const filters = {
+                        doctorId: 2, // або отримуй його динамічно
+                        dateRangeStart: range.start,
+                        dateRangeEnd: range.end,
+                    };
+
+                    const url = new URL(`${apiBaseUrl}/visit-request/assigned`);
+                    Object.entries(filters).forEach(([key, value]) => {
+                        if (value) url.searchParams.append(key, value);
+                    });
+
+                    const newEvents = await performRequest(url.toString());
+                    
+                    
+                    
+                    eventsService.set(newEvents.map(request => ({
+                        id: String(request.visitRequestId),
+                        title: request.visitRequestType,
+                        start: formatDateTime(request.startDateTime),
+                        end: formatDateTime(request.endDateTime),
+                        location: request.address,
+                        description: "Double click on the visit to get more information",
+                        calendarId: "event",
+                    })))
+                    
+                } catch (error) {
+                    console.error("Failed to fetch new events:", error);
+                }
+            },
+
+            onDoubleClickEvent: (calendarEvent) => {
+                navigate(`../visits/${calendarEvent.id}`);
+            },
+          
+        },
+        calendars: {
+            event: {
+                colorName: "event",
+                lightColors: {
+                    main: "#1cf9b0",
+                    container: "#dafff0",
+                    onContainer: "#004d3d",
+                },
+                darkColors: {
+                    main: "#c0fff5",
+                    onContainer: "#e6fff5",
+                    container: "#42a297",
+                },
+            },
+        },
+        plugins: [eventsService, createEventModalPlugin()],
+    });
+    
+    
+
+    return (
+        <StyledPaper width="90%" height="83vh" marginLeft="2.5rem">
+            <ScheduleXCalendar calendarApp={calendar} />
+        </StyledPaper>
+    );
+}
---
+++ DoctorOnCallFrontend/src/pages/current_visits/CurrentVisits.jsx
@@ -0,0 +1,63 @@
+﻿import StyledPaper from "../../components/StyledPaper.jsx";
+import { useState} from "react";
+import { TabContext, TabPanel } from "@mui/lab";
+import StyledTabList from "../../components/StyledTabList.jsx";
+import styles from "./current_visits.module.css";
+import arrow from "../../assets/arrow.svg";
+import StyledTab from "../../components/StyledTab.jsx";
+import { APIProvider} from "@vis.gl/react-google-maps";
+import RouteMap from "../../components/RouteMap.jsx";
+import {useLoaderData} from "react-router-dom";
+import VisitRequestItem from "../../components/visit_request_item/VisitRequestItem.jsx";
+import {Stack} from "@mui/material";
+
+
+export default function CurrentVisits() {
+    const {hospitalCoords, currentVisitRequests} = useLoaderData();
+    const [activeTab, setActiveTab] = useState("visits");
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+    const start = hospitalCoords;
+
+    const waypoints = currentVisitRequests.map(visitRequest => visitRequest.visitCoords)
+    const handleChange = (event, newValue) => {
+        setActiveTab(newValue);
+    };
+
+    const handleReturnBack = () => {
+        window.history.back();
+    };
+
+    return (
+        <StyledPaper width="70%" height="90%" marginLeft="10rem">
+            <TabContext value={activeTab}>
+                <div className={styles.header}>
+                    <StyledTabList onChange={handleChange} centered>
+                        <button className={styles.return_btn} onClick={handleReturnBack}>
+                            <img src={arrow} alt="arrow" />
+                        </button>
+                        <StyledTab label="Visits" value="visits" fontSize="1.25rem" />
+                        <StyledTab label="Route" value="route" fontSize="1.25rem" />
+                    </StyledTabList>
+                </div>
+                <TabPanel value="visits" sx={{height: '90%'}}>
+                    <ul type='none' className={styles.visit_requests_list}>
+                        {currentVisitRequests.length !== 0 ? currentVisitRequests.map(visitRequest => <li key={visitRequest.id}>
+                            <VisitRequestItem visitRequest={visitRequest} />
+                        </li>):
+                            <Stack width="100%" justifyContent="center" height='100%' alignItems="center">
+                                <h2>You have no visits for today </h2>
+                            </Stack>}
+                    </ul>
+                </TabPanel>
+                <TabPanel value="route" sx={{ padding: 0 }}>
+                    <APIProvider apiKey={apiKey}>
+                        <RouteMap start={start} waypoints={waypoints} />
+                    </APIProvider>
+                </TabPanel>
+              
+            </TabContext>
+        </StyledPaper>
+    );
+}
+
