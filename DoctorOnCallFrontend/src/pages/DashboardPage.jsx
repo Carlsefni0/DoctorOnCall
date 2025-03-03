6587d9a11375cd5a43edea3e8aad7097a87b030e
Nazar <example"gmail.com>
2025-03-03 13:52:27 +0200

    feat: application layout and error handling page created

---
+++ DoctorOnCallFrontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,20 @@
+﻿import styles from "../styles/dashboard_style.module.css";
+import Header from "../components/header/Header.jsx";
+import Sidebar from "../components/sidebar/Sidebar.jsx";
+import { Outlet } from "react-router-dom";
+
+export default function Dashboard() {
+    return (
+        <div className={styles.dashboard_page_container}>
+            <div className={styles.dashboard_header}>
+                <Header />
+            </div>
+            <div className={styles.dashboard_sidebar}>
+                <Sidebar />
+            </div>
+            <div className={styles.dashboard_content}>
+                <Outlet />
+            </div>
+        </div>
+    );
+}
---
+++ DoctorOnCallFrontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,31 @@
+﻿import styles from "../styles/error_page_styles.module.css";
+import {useLocation, useRouteError} from "react-router-dom";
+import StyledButton from "../components/StyledButton.jsx";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    const location = useLocation();
+    const errorMessage = error?.message || location.state?.message || "Something went wrong.";
+    const errorDetails =
+        error?.data && typeof error.data === "string"
+            ? JSON.parse(error.data)
+            : null;
+
+    return (
+        <div className={styles.error_page_container}>
+            <div className={styles.error_message_wrapper}>
+                <h1>Ooops!</h1>
+                <p>{errorMessage}</p>
+                {errorDetails?.details && (
+                    <div className={styles.error_details}>
+                        <h3>Details:</h3>
+                        <pre>{errorDetails.details}</pre>
+                    </div>
+                )}
+                <StyledButton onClick={() => window.history.back()} width='40%' marginTop='2rem'>
+                    Go Back
+                </StyledButton>
+            </div>
+        </div>
+    );
+}
